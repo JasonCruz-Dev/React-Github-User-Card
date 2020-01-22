bf07b92ecd4471c8ed14c14c3149caa31810a6cb
Jason Cruz <badmonkey.dev@gmail.com>
2020-01-21 20:31:29 -0600

    added some folders and files

--- user-card/src/App.js
+++ user-card/src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Card from "./Components/Card";
+import Search from "./Components/Search";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    searchTerm: "",
+    user: {},
+    followers: []
+  };
+
+  componentDidMount() {
+    this.getUser("JasonCruz-Dev");
+  }
+
+  getUser = searchInput => {
+    this.setState({ searchTerm: searchInput });
+    fetch(`https://api.github.com/users/${searchInput}`)
+      .then(res => res.json())
+      .then(res => {
+        this.setState({ user: res });
+      })
+      .catch(err => console.log(err, "Search Input Error"));
+  };
+
+  getFollowers = () => {
+    fetch(`https://api.github.com/users/${this.state.searchTerm}/followers`)
+      .then(res => res.json())
+      .then(res => {
+        this.setState({
+          followers: res,
+          seearchTerm: res
+        });
+      })
+      .catch(err => console.log(err, "Following Error"));
+  };
+
+  render() {
+    return (
+      <div className='app'>
+        <h1 className='title'>React Github User Card</h1>
+        <Search getUser={this.getUser} getFollowers={this.getFollowers} />
+        <Card
+          user={this.state.user}
+          followers={this.state.followers}
+          getFollowers={this.getFollowers}
+        />
+      </div>
+    );
+  }
 }
-
-export default App;
